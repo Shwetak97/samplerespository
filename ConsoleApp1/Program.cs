6e5e4c2caf94abd7958a73e8c8ca9dc1622d80b5
Shweta Kale <shwetakale25797@gmail.com>
2021-06-07 14:33:56 +0530

    creating repository

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long Total = 0;
+            for (int count = 0; count < 5; count++)
+            {
+                Console.WriteLine("Enter Mark" + count);
+                int mark = Convert.ToInt32(Console.ReadLine());
+                Total = Total + mark;
+            }
+            Console.WriteLine("Total Marks:" + Total);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goodNum1 = 0;
+            int goodNum2 = 0;
+        CollectNumber1:
+            Console.WriteLine("Enter Number1");
+            var Num1 = Console.ReadLine();
+            if (Num1 == "")
+            {
+                Console.WriteLine("Hi User please Enter a value again");
+                goto CollectNumber1;
+            }
+            else
+            {
+                goodNum1 = Convert.ToInt32(Num1);
+            }
+        GetNumber2:
+            Console.WriteLine("Enter Number2");
+            var Num2 = Console.ReadLine();
+            if(Num2=="")
+            {
+                Console.WriteLine("Hi User please enter a value again");
+                goto GetNumber2;
+            }
+            else
+            {
+                goodNum2 = Convert.ToInt32(Num2);
+            }
+
+            var res = goodNum1 + goodNum2;
+            Console.WriteLine(res);
+            Console.ReadLine();
+
+        }
+    }
+}
